4f9b8a99a4af3f52e4ddbd6f4f5a1774aa0b3d32
Isaac Méndez Navarrete <isaacmn2103@gmail..com>
2025-07-04 12:42:50 -0600

    test

--- Chess.js
+++ Chess.js
@@ -1,2 +1,11 @@
-let saludo="Hola mundo";
-console.log(saludo);
+function move({part,position}, allPieces = []){
+const [col, row] = position;
+const colLetters="ABCDEFGH";
+
+function isOccupied(pos){
+    return allPieces.some(piece => piece.position[0] === pos[0] && piece.position[1] === pos[1]);
+}
+
+
+
+}
