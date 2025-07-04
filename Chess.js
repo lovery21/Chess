8fa19a22202a990c93dc6c637f63cc0cbe71478e
Isaac Méndez Navarrete <isaacmn2103@gmail..com>
2025-07-04 16:45:23 -0600

    pawn eat

--- Chess.js
+++ Chess.js
@@ -1,36 +1,47 @@
-function move({part,position}, allPieces = []){
-const [col, row] = position;
-const colLetters="ABCDEFGH";
-//Piece in this position
-function isOccupied(pos){
-    return allPieces.some(piece => piece.position[0] === pos[0] && piece.position[1] === pos[1]);
-}
+function move({ part, position }, allPieces = []) {
+    const [col, row] = position;
+    const colLetters = "ABCDEFGH";
+    //Piece in this position
+    function isOccupied(pos) {
+        return allPieces.some(piece => piece.position[0] === pos[0] && piece.position[1] === pos[1]);
+    }
 
     //Search enemies pieces
-    function enemiesPieces(pos, color){
-const target=allPieces.find(p=>p.position[0] === pos[0] && p.position[1] === pos[1]);
-return target && target.color !==color;
+    function enemiesPieces(pos, color) {
+        const target = allPieces.find(p => p.position[0] === pos[0] && p.position[1] === pos[1]);
+        return target && target.color !== color;
 
     }
 
-      // Converce letter in number to move in columns
-    const colIndex=collIndexOf(col);
-    let moves=[];
+    // Converce letter in number to move in columns
+    const colIndex = collIndexOf(col);
+    let moves = [];
 
     //Pawn movement
-    if(part === "pawn"){
-        const oneStep=[col,row+1];
+    if (part === "pawn") {
+        const oneStep = [col, row + 1];
         // Check if the next step is not occupied
-        if(!isOccupied(oneStep)){
+        if (!isOccupied(oneStep)) {
             moves.push(oneStep);
         }
-}
-//first move double step
-if(row===2){
-    const twoSteps=[col.row+2];
-    if(!isOccupied(twoSteps)){
-        moves.push(twoSteps);
+    
+    //first move double step
+    if (row === 2) {
+        const twoSteps = [col.row + 2];
+        if (!isOccupied(twoSteps)) {
+            moves.push(twoSteps);
+        }
     }
-}
 
+    //eat diagonal
+    const eatleft=[colLetters[colIndex - 1], row + 1];
+    const eatRight = [colLetters[colIndex + 1], row + 1];
+    if (enemiesPieces(eatleft, "white")) {
+        moves.push(eatleft);
+    }
+    if (enemiesPieces(eatRight, "white")) {
+        moves.push(eatRight);
+    }
+
+}
 }
